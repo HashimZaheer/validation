d7b92c7c084f61d0aad0f375f424aa1f9ea79397
HashimZaheer <zabdulhashim@gmail.com>
2017-12-06 16:33:49 +0530

    validation

---
+++ we.js
@@ -0,0 +1,16 @@
+Vue.use(VeeValidate);
+
+var app = new Vue({
+	el: '#app',
+    data: {
+    	firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    },
+    methods: {
+    	sign: function(e) {
+        	//Sign User Up
+        }
+    }
+});
